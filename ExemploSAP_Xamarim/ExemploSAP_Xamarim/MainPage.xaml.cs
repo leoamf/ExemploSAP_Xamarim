26411b303d833c16b2289508e8c32a616d52e0fc
aluno <aluno@LABMAC09.local>
2018-10-19 21:41:02 -0300

    started

---
+++ ExemploSAP_Xamarim/ExemploSAP_Xamarim/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Xamarin.Forms;
+
+#if __IOS__
+using UIKit;
+#elif __ANDROID__
+using Android.OS;
+#endif
+
+namespace ExemploSAP_Xamarim
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            #if __IOS__
+                UIDevice devide =new UIDevice();
+                lblInfo.Text = devide.Model;
+            #elif __ANDROID__ 
+                lblInfo.Text = Build.Manufacturer;
+            #endif 
+        }
+    }
+}
